7c2b31ed6e4a8fb138e4f7e2b3d34272b7639e5a
zsoooti96 <magyar.zsolti96@gmail.com>
2026-04-12 22:28:25 +0200

    fix docker port + enable swagger in production

--- backend/Program.cs
+++ backend/Program.cs
@@ -18,11 +18,9 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 app.UseCors("allowAll");
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 app.MapControllers();
