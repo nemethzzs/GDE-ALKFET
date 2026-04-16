5d2e5f08146602703ab2388af566e908f09d0529
hunorgg <vrshunor@gmail.com>
2026-04-16 11:05:42 +0200

    feat: room dropdown fix, stable select binding, delete + validation improvements

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { NgFor, NgIf } from '@angular/common';
@@ -13,31 +13,42 @@ import { FormsModule } from '@angular/forms';
 
     <button (click)="load()">Foglalások betöltése</button>
 
- <ul *ngIf="data">
-  <li *ngFor="let f of data">
-    Szoba: {{ f.szobaszam }} |
-    Név: {{ f.vendegNev }} |
-    Dátum: {{ f.datum }} |
-    Ár: {{ f.ar }}
+    <ul *ngIf="data">
+      <li *ngFor="let f of data">
+        Szoba: {{ f.szobaszam }} |
+        Név: {{ f.vendegNev }} |
+        Dátum: {{ f.datum }} |
+        Ár: {{ f.ar }}
 
-    <button (click)="torol(f)"> Törlés</button>
-  </li>
-</ul>
+        <button (click)="torol(f)">Törlés</button>
+      </li>
+    </ul>
 
     <hr>
 
     <h3>Új foglalás</h3>
 
-    <input placeholder="Szobaszám" [(ngModel)]="szobaszam">
+    <!-- FIX: nincs *ngIf -->
+    <select [(ngModel)]="szobaszam">
+      <option *ngIf="szobak.length === 0" disabled>
+        Szobák betöltése...
+      </option>
+
+      <option *ngFor="let s of szobak" [value]="s.szobaszam">
+        {{ s.szobaszam }} - {{ s.ar }} Ft
+      </option>
+    </select>
+
     <input placeholder="Név" [(ngModel)]="nev">
     <input type="date" [(ngModel)]="datum">
 
     <button (click)="foglal()">Foglalás</button>
   `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   data: any;
+  szobak: any[] = [];
 
   szobaszam: number = 1;
   nev: string = '';
@@ -45,44 +56,72 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.load();
+    this.loadSzobak();
+  }
+
   load() {
     this.http.get('http://localhost:5000/api/hotel/foglalasok')
       .subscribe(res => this.data = res);
   }
 
-foglal() {
-  this.http.post('http://localhost:5000/api/hotel/foglal', {
-    szobaszam: this.szobaszam,
-    vendegNev: this.nev,
-    datum: this.datum
-  }).subscribe({
-    next: () => {
-      alert("Foglalás sikeres!");
-      this.load();
-    },
-    error: err => {
-      alert(err.error); 
+  loadSzobak() {
+    this.http.get('http://localhost:5000/api/hotel/szobak')
+      .subscribe({
+        next: (res: any) => {
+          console.log("SZOBÁK:", res);
+          this.szobak = res;
+
+          if (this.szobak.length > 0) {
+            this.szobaszam = this.szobak[0].szobaszam;
+          }
+        },
+        error: err => {
+          console.error("Szobák betöltése hiba:", err);
+        }
+      });
+  }
+
+  foglal() {
+
+    if (!this.nev || !this.datum) {
+      alert("Minden mezőt tölts ki!");
+      return;
     }
-  });
+
+    this.http.post('http://localhost:5000/api/hotel/foglal', {
+      szobaszam: this.szobaszam,
+      vendegNev: this.nev,
+      datum: this.datum
+    }).subscribe({
+      next: () => {
+        alert("Foglalás sikeres!");
+        this.load();
+      },
+      error: err => {
+        alert(err.error);
+      }
+    });
+  }
+
+  torol(f: any) {
+
+    if (!confirm("Biztos törlöd?")) return;
+
+    const datum = new Date(f.datum).toISOString().split('T')[0];
+
+    this.http.delete(
+      `http://localhost:5000/api/hotel/torles?szobaszam=${f.szobaszam}&datum=${datum}`
+    ).subscribe({
+      next: () => {
+        alert("Foglalás törölve!");
+        this.load();
+      },
+      error: err => {
+        console.error(err);
+        alert("Hiba történt törléskor!");
+      }
+    });
+  }
 }
-torol(f: any) {
-
-  if (!confirm("Biztos törlöd?")) return;
-
-  const datum = new Date(f.datum).toISOString().split('T')[0];
-
-  this.http.delete(
-    `http://localhost:5000/api/hotel/torles?szobaszam=${f.szobaszam}&datum=${datum}`
-  ).subscribe({
-    next: () => {
-      alert("Foglalás törölve!");
-      this.load();
-    },
-    error: err => {
-      console.error(err);
-      alert("Hiba történt törléskor!");
-    }
-  });
-}
-  };
-  
